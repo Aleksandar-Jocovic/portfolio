71f4984706868155e048cf99b4b14b6a7137c8dc
aleksandar <jocovic099@gmail.com>
2020-02-23 11:02:40 +0100

    cards
    adding card in about page

--- javascripts/show-on-scroll.js
+++ javascripts/show-on-scroll.js
@@ -4,6 +4,11 @@ var scroll = window.requestAnimationFrame ||
   };
 var elementsToShow = document.querySelectorAll('.show-on-scroll');
 var imgBio = document.querySelectorAll('.imgbiojs')
+var contactForm = document.querySelectorAll('.contact-info-formjs');
+var cardA = document.querySelector('.cardA');
+var cardB = document.querySelector('.cardB');
+var cardC = document.querySelector('.cardC');
+
 
 function loop() {
 
@@ -21,7 +26,66 @@ function loop() {
     } else {
       element.classList.remove('img-bio-apprear');
     }
+
   })
+
+  contactForm.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('contact-form-apprear');
+    } else {
+      element.classList.remove('contact-form-apprear');
+    }
+
+  })
+
+
+  if (isElementInViewport(cardA)) {
+    var cardImg = document.querySelector('.card-img');
+    var cardVrap = document.querySelector('.card-img-vrap');
+    var cardText = document.querySelector('.card-text');
+
+    cardImg.classList.add('card-img2');
+    cardText.classList.add('card-text2');
+    cardVrap.classList.add('card-img-vrap2');
+
+  } else {
+
+  }
+
+
+
+
+  if (isElementInViewport(cardB)) {
+    var cardImgB = document.querySelector('.card-imgB');
+    var cardVrapB = document.querySelector('.card-img-vrapB');
+    var cardTextB = document.querySelector('.card-textB');
+
+    cardImgB.classList.add('card-img2');
+    cardTextB.classList.add('card-text2');
+    cardVrapB.classList.add('card-img-vrap2');
+
+  } else {
+
+  }
+
+  if (isElementInViewport(cardC)) {
+    var cardImgC = document.querySelector('.card-imgC');
+    var cardVrapC = document.querySelector('.card-img-vrapC');
+    var cardTextC = document.querySelector('.card-textC');
+
+    cardImgC.classList.add('card-img2');
+    cardTextC.classList.add('card-text2');
+    cardVrapC.classList.add('card-img-vrap2');
+
+  }
+
+
+
+
+
+
+
+
 
   //img and bio
 
@@ -37,9 +101,9 @@ function isElementInViewport(el) {
   }
   var rect = el.getBoundingClientRect();
   return (
-    (rect.top <= 0 && rect.bottom >= 0) ||
-    (rect.bootom >= (window.innerHeight || document.documentElement.clientHeight) &&
+    (rect.top <= 0 && rect.bottom >= 0 - rect.height / 2) ||
+    (rect.bootom - rect.height / 2 >= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
-    (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    (rect.top >= 0 && rect.bottom - rect.height / 2 <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
