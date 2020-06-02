545c5f760d32a6c6f8f1e12e3cc6b3d255cd464f
aleksandar <jocovic099@gmail.com>
2020-06-02 19:14:11 +0200

    critical css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,109 @@
+// back to top button and annimations
+var button = document.getElementById('back-to-top-btn');
+window.addEventListener('scroll', scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 200) {
+    if (!button.classList.contains('btnIn')) {
+      button.classList.remove('btnOut');
+      button.classList.add('btnIn');
+      button.style.display = 'block';
+    }
+  } else {
+    if (button.classList.contains('btnIn')) {
+      button.classList.remove('btnIn');
+      button.classList.add('btnOut');
+      setTimeout(function () {
+        button.style.display = 'none';
+      }, 250);
+    }
+  }
+}
+
+button.addEventListener('click', smoothScroll);
+
+function smoothScroll() {
+  var targetPosition = 0;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var duration = 450;
+  let start = null;
+
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    window.scrollTo(
+      0,
+      easeInOutCubic(progress, startPosition, distance, duration)
+    );
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t * t + b;
+  t -= 2;
+  return (c / 2) * (t * t * t + 2) + b;
+}
+
+// show animations on scrooll
+var scroll = window.requestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60)
+  };
+
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+var imgBio = document.querySelectorAll('.imgbiojs')
+var contactForm = document.querySelectorAll('.contact-info-formjs');
+
+const tech = document.querySelector('.technologies-vrap')
+const techSmall = document.querySelector('.technologies-vrap-small')
+
+
+function loop() {
+
+  imgBio.forEach(function (element) {
+    if (isElementInViewport(element) && !element.classList.add('img-bio-apprear')) {
+      element.classList.add('img-bio-apprear');
+    }
+  })
+
+  contactForm.forEach(function (element) {
+    if (isElementInViewport(element) && !element.classList.contains('contact-form-apprear')) {
+      element.classList.add('contact-form-apprear');
+    }
+  })
+
+  if (isElementInViewport(tech) && tech.classList.contains('on-appear')) {
+    tech.classList.remove('on-appear');
+  }
+
+  if (isElementInViewport(techSmall) && techSmall.classList.contains('on-appear')) {
+    techSmall.classList.remove('on-appear');
+  }
+
+  scroll(loop);
+}
+
+loop();
+
+
+function isElementInViewport(el) {
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0 - rect.height / 2) ||
+    (rect.bootom - rect.height / 2 >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 && rect.bottom - rect.height / 2 <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
 //select dom items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
